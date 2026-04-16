593a64056dd559972774d0e305909742b45d11c4
rmdkarhdwn <dkgkrlrnlvksgsk@gmail.com>
2026-04-16 14:40:53 +0900

    feat: add board state model for 8x8 grid

--- lib/game/dual_blocks_game.dart
+++ lib/game/dual_blocks_game.dart
@@ -3,12 +3,18 @@ import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 import 'components/dual_blocks_renderer.dart';
+import 'config/game_constants.dart';
+import 'models/cell_state.dart';
 import 'models/game_layout.dart';
 import 'systems/layout_system.dart';
 
 class DualBlocksGame extends FlameGame {
   late GameLayout layout;
   int score = 0;
+  final List<List<CellState>> board = List.generate(
+    GameConstants.boardSize,
+    (_) => List.generate(GameConstants.boardSize, (_) => CellState.empty),
+  );
 
   @override
   Future<void> onLoad() async {
---
+++ lib/game/models/cell_state.dart
@@ -0,0 +1,4 @@
+enum CellState {
+  empty,
+  filled,
+}
